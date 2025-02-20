2b1316d7524e29e453b4227885cb11555fd25229
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2025-02-20 18:12:19 +0500

    master

---
+++ first/crewai_core.py
@@ -0,0 +1,12 @@
+# crewai_core.py
+class Agent:
+    pass  # Your implementation here
+
+class Crew:
+    pass  # Your implementation here
+
+class Process:
+    pass  # Your implementation here
+
+class Task:
+    pass  # Your implementation here
